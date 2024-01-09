6818b7fece6eebeb93566118d7580cbfa4e239bc
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2024-01-09 17:10:40 +0530

    Plaindrome program

---
+++ FrontendDSA.js
@@ -0,0 +1,9 @@
+// Palindrome
+function checkPalindrome(number) {
+    return number < 0 ? false : (number === +number.toString().split("").reverse().join(""));
+}
+
+console.log(checkPalindrome(121));
+console.log(checkPalindrome(-121));
+
+
